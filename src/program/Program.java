2626317699ab5d1d51cff55c3bd9d0509c518307
nhan1906 <nhantx.1906@gmail.com>
2018-06-08 06:01:17 +0700

    Initialize project and run music background

---
+++ src/program/Program.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import program.utils.ConstrantStrings;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.rmi.runtime.Log;
+
+/**
+ *
+ * @author James
+ */
+public class Program {
+
+    
+    public static void main(String[] args) {
+        runMusicBackground();
+    }
+
+    private static void runMusicBackground() {
+        try{
+            InputStream is = new FileInputStream(new File(ConstrantStrings.dirFileMusic)); // throws FileNotFoundException
+            AudioStream as = new AudioStream(is);
+            AudioPlayer.player.start(as);
+        }
+        catch(Exception e){ // 
+            // Xu ly loi khong nhap duoc file
+        }
+    }
+    
+}
